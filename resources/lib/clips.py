4cb497195e7ce25684560cf259ab1351ed958d2b
Maven85 <janis.semper@googlemail.com>
2020-05-04 11:52:03 +0200

    sync with lastest version

--- resources/lib/clips.py
+++ resources/lib/clips.py
@@ -1,48 +1,60 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+from __future__ import unicode_literals
+from kodi_six.utils import py2_decode
 import json
 import time
 import base64
-import urllib
-# cryptopy
-from crypto.cipher import aes_cbc
-# pycrypto
-# from Crypto.Cipher import AES
+from Cryptodome.Cipher import AES
 
-skygo = None
-
-secret_key = 'XABD-FHIM-GDFZ-OBDA-URDG-TTRI'
-aes_key = ['826cf604accd0e9d61c4aa03b7d7c890', 'da1553b1515bd6f5f48e250a2074d30c']
+try:
+    from urllib.parse import urlencode
+except:
+    from urllib import urlencode
 
 
-def getClipToken(content):
-    clipType = 'FREE'
-    if content == 'ENTITLED USER' or content == 'SUBSCRIBED USER':
-        clipType = 'NOTFREE'
-    timestamp = str(time.time()).replace('.', '')
-    url = 'https://www.skygo.sky.de/SILK/services/public/clipToken?clipType=' + clipType + '&product=SG&platform=web&version=12354=&_' + timestamp
-    r = skygo.session.get(url)
-    return json.loads(r.text[3:len(r.text) - 1])
+class Clips:
+
+    secret_key = 'XABD-FHIM-GDFZ-OBDA-URDG-TTRI'
+    aes_key = ['826cf604accd0e9d61c4aa03b7d7c890', 'da1553b1515bd6f5f48e250a2074d30c']
 
 
-def buildClipUrl(url, token):
-    # pyCrypto
-    # dec = AES.new(aes_key[0].decode('hex'), AES.MODE_CBC, aes_key[1].decode('hex'))
-    # path = dec.decrypt(base64.b64decode(token['tokenValue']))
-    # query = token['tokenName'] + '=' + path2[0:len(path2)-7]
-    #
-    # cryptopy
-    dec = aes_cbc.AES_CBC(key=aes_key[0].decode('hex'), keySize=16)
-    path = dec.decrypt(base64.b64decode(token['tokenValue']), iv=aes_key[1].decode('hex'))
-    query = token['tokenName'] + '=' + path
-    return url + '?' + query
+    def __init__(self, skygo):
+
+        self.skygo = skygo
 
 
-def playClip(clip_id):
-    if skygo.login():
-        clip_info = skygo.getClipDetails(clip_id)
-        token = getClipToken(clip_info['content_subscription'])
-        manifest = buildClipUrl(clip_info['videoUrlMSSProtected'], token)
+    def getClipToken(self, content):
+        clipType = 'FREE'
+        if content == 'ENTITLED USER' or content == 'SUBSCRIBED USER':
+            clipType = 'NOTFREE'
+        timestamp = str(time.time()).replace('.', '')
+        url = 'https://www.skygo.sky.de/SILK/services/public/clipToken?{0}'.format(urlencode({
+            'clipType': clipType,
+            'version': '12354',
+            'platform': 'web',
+            'product': 'SG'
+        }))
+        url = '{0}&_{1}'.format(timestamp)
 
-        skygo.play(manifest, clip_info['package_code'])
+        r = self.skygo.session.get(url)
+        if common.get_dict_value(r.headers, 'content-type').startswith('application/json'):
+            return json.loads(py2_decode(r.text[3:len(r.text) - 1]))
+        else:
+            None
+
+
+    def buildClipUrl(self, url, token):
+        dec = AES.new(self.aes_key[0].decode('hex'), AES.MODE_CBC, iv=self.aes_key[1].decode('hex'))
+        path = dec.decrypt(base64.b64decode(token['tokenValue']))
+        query = '{0}={1}'.format(token['tokenName'], path)
+        return '{0}?{1}'.format(url, query)
+
+
+    def playClip(self, clip_id):
+        if self.skygo.login():
+            clip_info = self.skygo.getClipDetails(clip_id)
+            token = getClipToken(clip_info['content_subscription'])
+            manifest = buildClipUrl(clip_info['videoUrlMSSProtected'], token)
+
+            self.skygo.play(manifest, clip_info['package_code'])
